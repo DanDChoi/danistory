6eef3b8ea51c6d8674719adbed22d7fdb4d4a88f
Dan <danchoi.dev@gmail.com>
2026-04-13 23:18:31 +0900

    vercel deploy error fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,7 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/common/Header";
 import { getAllPosts } from "@/lib/blog";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Danistory",
@@ -30,9 +19,7 @@ export default function RootLayout({
     .sort((a, b) => a.localeCompare(b));
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className="antialiased">
         <Header categories={categories} />
         {children}
       </body>
