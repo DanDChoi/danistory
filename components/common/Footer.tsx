f756c326a44b8cb4616a02328e9136f2accc460c
Dan <danchoi.dev@gmail.com>
2026-04-18 23:53:16 +0900

    footer 영역 생성

---
+++ components/common/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="mt-auto border-t border-gray-200 dark:border-gray-100">
+      <div className="max-w-4xl mx-auto px-4 py-8 text-center text-sm text-gray-400">
+        {/* 추후 내용 추가 예정 */}
+      </div>
+    </footer>
+  );
+}
